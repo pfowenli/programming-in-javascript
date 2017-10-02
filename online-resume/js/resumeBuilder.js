892d5b01f547c33350f0d6948111542fb43aad18
Owen P. F. Li <pfowenli@gmail.com>
2017-10-02 23:57:44 +0800

    modify projects

--- online-resume/js/resumeBuilder.js
+++ online-resume/js/resumeBuilder.js
@@ -5,20 +5,22 @@ This is empty on purpose! Your code to build the resume will go here.
 var work = {
 	"jobs": [
 	    {
-            "employer": "National Cheng Kung Univesity Hospital",
-            "title": "Web Administrator",
-            "location": "Tainan City",
-            "years": 1,
-            "dates": "Jul 2014 - Jun 2015",
-            "description": "webpage design"
-	    },
-	    {
             "employer": "Insight Genomics Co., Ltd",
             "title": "Data Analyst",
             "location": "Tainan City",
             "years": 1,
             "dates": "Oct 2016 - current",
-            "description": "bioinformatics analysis and web system development"
+            "description": "bioinformatics analysis and web development",
+            "URL": "http://www.i-genomics.com.tw"
+	    },
+	    {
+            "employer": "National Cheng Kung Univesity Hospital",
+            "title": "Web Administrator",
+            "location": "Tainan City",
+            "years": 1,
+            "dates": "Jul 2014 - Jun 2015",
+            "description": "webpage design",
+            "URL": "http://www.hosp.ncku.edu.tw"
 	    }
 	]
 };
@@ -26,16 +28,25 @@ var work = {
 var projects = {
     "projects": [
         {
-            "title": "title 1",
-            "dates": "date 1",
-            "description": "description 1",
-            "images": ["images/197x148.gif"]
+            "title": "Digits Data Classification",
+            "dates": "Oct 2017",
+            "description": "Scikit Learn",
+            "URL": "http://nbviewer.jupyter.org/github/pfowenli/data-science-in-python/blob/b18ba4ecdd6336ba680b9380c414b055f529cc27/Scikit-Learn/scikit-learn-digits-dataset.ipynb"
+            //"images": ["images/197x148.gif"]
         },
         {
-            "title": "title 2",
-            "dates": "date 2",
-            "description": "description 2",
-            "images": ["images/197x148.gif", "images/197x148.gif"]
+            "title": "Just Java - User Input",
+            "dates": "Sep 2017",
+            "description": "Android application development",
+            "URL": "https://github.com/pfowenli/android-app-development/tree/master/basics/JustJava"
+            //"images": ["images/197x148.gif"]
+        },
+        {
+            "title": "Belgian Traffic Signs Prediction Using Neural Networks",
+            "dates": "Aug 2017",
+            "description": "TensorFlow",
+            "URL": "http://nbviewer.jupyter.org/github/pfowenli/data-science-in-python/blob/b18ba4ecdd6336ba680b9380c414b055f529cc27/TensorFlow/tensorflow-for-beginners.ipynb"
+            //"images": ["images/197x148.gif"]
         }
     ]
 };
@@ -50,41 +61,67 @@ var bio = {
 	    location: "Tainan City"
 	},
 	"biopic": "images/me.jpg ",
-	"skills": ["data analytics", "web development"]
+	"skills": [
+	    "programming language - Java, JavaScript, Python", 
+	    "Java framework - Spring, Hibernate",
+	    "JavaScript library - jQuery",
+	    "Python data analytcis package - Matplotlib, Scikit-learn, Pandas, Tensorflow", 
+	    "database - MySQL, PostgreSQL, MongoDB",
+	    "mobile application - Android",
+	    "version control - git"
+	]
 };
 
 var education = {
 	"schools": [
 	    {
-	    	"name": "National Taiwan University of Science and Technology",
-	    	"location": "Taipei City",
-	    	"degree": "BA",
-	    	"years": 3.5,
-	    	"dates": 2013,
-	    	"majors": ["Electrical Engineering"],
-	    	"url": "http://www.ntust.edu.tw"
-	    },
-	    {
 	    	"name": "National Cheng Kung University",
 	    	"location": "Tainan City",
 	    	"degree": "Masters",
 	    	"years": 2.5,
-	    	"dates": 2015,
+	    	"dates": "Feb 2013 - Jun 2015",
 	    	"majors": ["Medical Informatics"],
 	    	"url": "http://www.ncku.edu.tw"
+	    },
+	    {
+	    	"name": "National Taiwan University of Science and Technology",
+	    	"location": "Taipei City",
+	    	"degree": "BA",
+	    	"years": 3.5,
+	    	"dates": "Sep 2009 - Jan 2013",
+	    	"majors": ["Electrical Engineering"],
+	    	"url": "http://www.ntust.edu.tw"
 	    }
 	],
 	"onlineCourses": [
 	    {
+            "title": "Data Visualization and D3.js",
+            "school": "Udacity",
+            "dates": "Sep 2017 - current",
+            "URL": "https://www.udacity.com/courses/ud507"
+	    },
+	    {
+            "title": "Android Basics: User Input",
+            "school": "Udacity & Google",
+            "dates": "Aug 2017 - Sep 2017",
+            "URL": "https://www.udacity.com/courses/ud836"
+	    },
+	    {
+            "title": "Android Basics: User Interface",
+            "school": "Udacity & Google",
+            "dates": "Sep 2017",
+            "URL": "https://www.udacity.com/courses/ud834"
+	    },
+	    {
             "title": "Intro to JavaScript",
             "school": "Udacity",
-            "dates": 2017,
+            "dates": "Aug 2017",
             "URL": "https://www.udacity.com/courses/ud803"
 	    },
 	    {
 	    	"title": "JavaScript Basics",
 	    	"school": "Udacity",
-            "dates": 2017,
+            "dates": "Aug 2017",
             "URL": "https://www.udacity.com/courses/ud804"
 	    }
 	]
@@ -103,14 +140,14 @@ if (bio.skills.length > 0) {
     $("#header").append(HTMLskillsStart);
     // loop over each skill
     for (var i = 0; i < bio.skills.length; i++) {
-    	$("#skills").append(HTMLskills.replace("%data%", bio.skills[i]));
+    	$("#skills").append(HTMLskills.replace("%data%", bio.skills[i]+"<br><br>"));
     }
 }
 
 work.display = function () {
     for (var i = 0; i < work.jobs.length; i++) {
 	    $("#workExperience").append(HTMLworkStart);
-        $(".work-entry:last").append(HTMLworkEmployer.replace("%data%", work.jobs[i].employer) + HTMLworkTitle.replace("%data%", work.jobs[i].title));
+        $(".work-entry:last").append(HTMLworkEmployer.replace("%data%", work.jobs[i].employer).replace("#", work.jobs[i].URL) + HTMLworkTitle.replace("%data%", work.jobs[i].title));
         $(".work-entry:last").append(HTMLworkDates.replace("%data%", work.jobs[i].dates));
         $(".work-entry:last").append(HTMLworkLocation.replace("%data%", work.jobs[i].location));
         $(".work-entry:last").append(HTMLprojectDescription.replace("%data%", work.jobs[i].description));
@@ -120,12 +157,14 @@ work.display = function () {
 projects.display = function () {
 	for (var i = 0; i < projects.projects.length; i++) {
 		$("#projects").append(HTMLprojectStart);
-		$(".project-entry:last").append(HTMLprojectTitle.replace("%data%", projects.projects[i].title));
+		$(".project-entry:last").append(HTMLprojectTitle.replace("%data%", projects.projects[i].title).replace("#", projects.projects[i].URL));
 		$(".project-entry:last").append(HTMLprojectDates.replace("%data%", projects.projects[i].dates));
 		$(".project-entry:last").append(HTMLprojectDescription.replace("%data%", projects.projects[i].description));
+		/*
 		for (var j = 0; j < projects.projects[i].images.length; j++) {
 			$(".project-entry:last").append(HTMLprojectImage.replace("%data%", projects.projects[i].images[j]));
 		}
+		*/
 	}
 };
 
@@ -136,6 +175,13 @@ education.display = function() {
 	    $(".education-entry:last").append(HTMLschoolDates.replace("%data%", education.schools[i].dates));
 	    $(".education-entry:last").append(HTMLschoolLocation.replace("%data%", education.schools[i].location));
 	    $(".education-entry:last").append(HTMLschoolMajor.replace("%data%", education.schools[i].majors.join(", ")));
+	}
+
+	for (var i = 0; i < education.onlineCourses.length; i++) {
+		$("#online").append(HTMLonlineStart);
+		$(".education-entry:last").append(HTMLonlineTitle.replace("%data%", education.onlineCourses[i].title).replace("#", education.onlineCourses[i].URL) + HTMLonlineSchool.replace("%data%", education.onlineCourses[i].school));
+	    $(".education-entry:last").append(HTMLonlineDates.replace("%data%", education.onlineCourses[i].dates));
+	    $(".education-entry:last").append(HTMLonlineURL.replace("%data%", "").replace("#", education.onlineCourses[i].URL));
 	}
 };
 
